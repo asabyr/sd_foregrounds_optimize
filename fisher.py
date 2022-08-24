790de072826056f0a07d3a992de9b0bc4ba1ee79
asabyr <76526893+asabyr@users.noreply.github.com>
2022-08-24 11:59:53 -0400

    added specter option & deprojecting modes function(+ python3 fixes)

--- fisher.py
+++ fisher.py
@@ -1,16 +1,23 @@
 import inspect
 import numpy as np
 from scipy import interpolate
-
+from scipy import linalg
+import sys
+import matplotlib.pyplot as plt
+sys.path.append('..') #path to fisher code
+sys.path.append('..') #path to project
+from NoiseFunctions import getnoise_nominal
 import spectral_distortions as sd
-import foregrounds as fg
+import foregrounds_fisher as fg
 ndp = np.float64
-
-
+#ndp=np.float128
+clight=299792458.
 class FisherEstimation:
     def __init__(self, fmin=7.5e9, fmax=3.e12, fstep=15.e9, \
                  duration=86.4, bandpass=True, fsky=0.7, mult=1., \
-                 priors={'alps':0.1, 'As':0.1}, drop=0, doCO=False):
+                 priors={'alps':0.1, 'As':0.1}, drop=0, doCO=False, instrument='pixie',\
+                  file_prefix='test',freq_bands=np.array([]), Ndet_arr=np.array([]),noisefile=False):
+
         self.fmin = fmin
         self.fmax = fmax
         self.bandpass_step = 1.e8
@@ -21,20 +28,70 @@ class FisherEstimation:
         self.mult = mult
         self.priors = priors
         self.drop = drop
+        self.file_prefix=file_prefix
 
-        self.setup()
+        if instrument=='specter':
+
+            self.freq_bands=freq_bands
+            self.Ndet_arr=Ndet_arr
+            self.noisefile=noisefile
+            self.center_frequencies, self.noise=self.specter_sensitivity()
+
+
+        elif instrument=='pixie':
+            self.set_frequencies()
+            self.noise = self.pixie_sensitivity()
+
+        else:
+            print("choose between pixie or specter")
+            return
+
         self.set_signals()
-        
+
         if doCO:
             self.mask = ~np.isclose(115.27e9, self.center_frequencies, atol=self.fstep/2.)
         else:
             self.mask = np.ones(len(self.center_frequencies), bool)
+            #print(self.mask)
+        #print(self.center_frequencies)
+        #print(self.noise)
         return
 
-    def setup(self):
-        self.set_frequencies()
-        self.noise = self.pixie_sensitivity()
-        return
+    @staticmethod
+    def cov_inv(cov,dim): #cov = dim x dim -sized cov matrix
+
+        Delta = np.diag(cov)
+        #print(np.shape(cov))
+        # construct correlation matrix R_ij = C_ij/sqrt(C_ii C_jj)
+        R = np.zeros((dim,dim))
+        for i in range(dim):
+            for j in range(dim):
+                R[i,j] = cov[i,j] / np.sqrt(cov[i,i]*cov[j,j])
+                R[j,i] = R[i,j] #symmetrize
+        # compute eigenvalues and eigenvectors of R
+        eigenValues,eigenVectors = np.linalg.eig(R)
+        # sort from largest to smallest eigenvalue
+        idx = eigenValues.argsort()[::-1]
+        eigenValues = eigenValues[idx]
+        eigenVectors = eigenVectors[:,idx]
+        print("eigen values")
+        print(eigenValues)
+        #print("eigen vectors")
+        #print(eigenVectors)
+        # deproject any eigenvalues that are a factor of thresh smaller than the largest one
+        eigenValues_inv = 1.0/eigenValues
+        N_deproj = 0 #count how many are deprojected
+        for i in range(len(eigenValues)):
+            if (eigenValues[i] < 0.):
+                eigenValues_inv[i] = 0.0
+                N_deproj += 1
+        # compute cov^-1 after having deprojected the modes associated with these eigenvalues
+        R_inv = np.inner(np.inner(eigenVectors,np.diag(eigenValues_inv)),eigenVectors)
+        Delta_fac = np.diag(1.0/np.sqrt(Delta))
+        cov_inv = np.inner(np.inner(Delta_fac,R_inv),Delta_fac)
+        print("number of deprojected:")
+        print(N_deproj)
+        return cov_inv
 
     def run_fisher_calculation(self):
         N = len(self.args)
@@ -43,19 +100,52 @@ class FisherEstimation:
             if k in self.args and self.priors[k] > 0:
                 kindex = np.where(self.args == k)[0][0]
                 F[kindex, kindex] += 1. / (self.priors[k] * self.argvals[k])**2
+        #print("fisher information matrix after priors & fiducial values")
+        #print(F.diagonal())
         normF = np.zeros([N, N], dtype=ndp)
         for k in range(N):
             normF[k, k] = 1. / F[k, k]
+
+        F_eigen_values=np.linalg.eig(F)[0]
+
+        # if np.all((F_eigen_values > 0.)):
+        #     self.cov = ((np.mat(normF, dtype=ndp) * np.mat(F, dtype=ndp)).I * np.mat(normF, dtype=ndp)).astype(ndp)
+        #
+        # else:
+        #     print("deprojecting negative modes")
+        #     normalizedF=np.mat(normF, dtype=ndp) * np.mat(F, dtype=ndp)
+        #     deproj_cov=FisherEstimation.cov_inv(normalizedF,N)
+        #     self.cov= (deproj_cov * np.mat(normF, dtype=ndp)).astype(ndp)
+
+            #deproj_cov=FisherEstimation.cov_inv(F,N)
+            #self.cov= deproj_cov
+
+        #self.cov=np.mat(F, dtype=ndp).I
         self.cov = ((np.mat(normF, dtype=ndp) * np.mat(F, dtype=ndp)).I * np.mat(normF, dtype=ndp)).astype(ndp)
-        #self.cov = np.mat(F, dtype=ndp).I 
+        # print("fractional")
+        # print(normalized)
+        #print("eigen normalized")
+        #print(np.linalg.eig(normalizedF)[0])
+        #print("eigen not-normalized")
+        #print(np.linalg.eig(F)[0])
+        #self.cov=np.mat(F, dtype=ndp).I
+        #self.cov=np.matmul(np.linalg.inv((np.matmul(normF,F))),normF)
+        #self.cov=(linalg.inv(normF.dot(F.T))).dot(normF.T)
+
+
+        #print("covariance matrix")
+        #print(self.cov.diagonal())
+        #self.cov = np.mat(F, dtype=ndp).I
         self.F = F
         self.get_errors()
+
         return
 
     def get_errors(self):
         self.errors = {}
         for k, arg in enumerate(self.args):
             self.errors[arg] = np.sqrt(self.cov[k,k])
+        #print(self.errors)
         return
 
     def print_errors(self, args=None):
@@ -68,7 +158,7 @@ class FisherEstimation:
     def set_signals(self, fncs=None):
         if fncs is None:
             fncs = [sd.DeltaI_mu, sd.DeltaI_reltSZ_2param_yweight, sd.DeltaI_DeltaT,
-                    fg.thermal_dust_rad, fg.cib_rad, fg.jens_freefree_rad, 
+                    fg.thermal_dust_rad, fg.cib_rad, fg.jens_freefree_rad,
                     fg.jens_synch_rad, fg.spinning_dust, fg.co_rad]
         self.signals = fncs
         self.args, self.p0, self.argvals = self.get_function_args()
@@ -84,10 +174,12 @@ class FisherEstimation:
 
     def band_averaging_frequencies(self):
         #freqs = np.arange(self.fmin + self.bandpass_step/2., self.fmax + self.fstep, self.bandpass_step, dtype=ndp)
-        freqs = np.arange(self.fmin + self.bandpass_step/2., self.fmax + self.bandpass_step + self.fmin, self.bandpass_step, dtype=ndp)
+        freqs = np.arange(self.fmin + self.bandpass_step/2., self.fmax + self.bandpass_step+self.fmin, self.bandpass_step, dtype=ndp)
         binstep = int(self.fstep / self.bandpass_step)
-        freqs = freqs[self.drop * binstep : (len(freqs) / binstep) * binstep]
-        centerfreqs = freqs.reshape((len(freqs) / binstep, binstep)).mean(axis=1)
+        #print(int((len(freqs) / binstep) * binstep))
+        freqs = freqs[self.drop * binstep : int((len(freqs) / binstep) * binstep)]
+        #print(len(freqs))
+        centerfreqs = freqs.reshape((int(len(freqs) / binstep), binstep)).mean(axis=1)
         #self.windowfnc = np.sinc((np.arange(binstep)-(binstep/2-1))/float(binstep))
         return freqs, centerfreqs, binstep
 
@@ -100,9 +192,18 @@ class FisherEstimation:
         if self.bandpass:
             N = len(self.band_frequencies)
             noise = 10. ** template(np.log10(self.band_frequencies)) / np.sqrt(skysr) * np.sqrt(15. / self.duration) * self.mult * 1.e26
-            return (noise.reshape(( N / self.binstep, self.binstep)).mean(axis=1)).astype(ndp)
+            return (noise.reshape((int( N / self.binstep), self.binstep)).mean(axis=1)).astype(ndp)
         else:
             return (10. ** template(np.log10(self.center_frequencies)) / np.sqrt(skysr) * np.sqrt(15. / self.duration) * self.mult * 1.e26).astype(ndp)
+
+    def specter_sensitivity(self):
+
+        center_frequencies, sens=getnoise_nominal(prefix=self.file_prefix, bands=self.freq_bands, dets=self.Ndet_arr, precompute=self.noisefile)
+        skysr = 4. * np.pi * (180. / np.pi) ** 2 * self.fsky
+
+        # print((center_frequencies).astype(ndp))
+        # print((sens/ np.sqrt(skysr) * np.sqrt(6./self.duration) * self.mult).astype(ndp))
+        return (center_frequencies).astype(ndp),(sens/ np.sqrt(skysr) * np.sqrt(6./self.duration) * self.mult).astype(ndp)
 
     def get_function_args(self):
         targs = []
@@ -118,20 +219,74 @@ class FisherEstimation:
     def calculate_fisher_matrix(self):
         N = len(self.p0)
         F = np.zeros([N, N], dtype=ndp)
+        #print(self.args)
+        #print(self.p0)
         for i in range(N):
+            #print(self.args[i])
+            #print(self.p0[i])
             dfdpi = self.signal_derivative(self.args[i], self.p0[i])
+            #print(dfdpi)
             dfdpi /= self.noise
+            #print(dfdpi)
+            # if dfdpi[self.mask].any() < 0.:
+            #     print(dfdpi)
+            #     print(self.args[i])
+            #     print(self.p0[i])
+
             for j in range(N):
                 dfdpj = self.signal_derivative(self.args[j], self.p0[j])
+                #print(self.args[j])
+                #print(self.p0[j])
+                #print(dfdpj)
                 dfdpj /= self.noise
+                #print(dfdpj)
+                # if dfdpj[self.mask].any() < 0.:
+                #     print(dfdpj)
+                #     print(self.args[j])
+                #     print(self.p0[j])
                 #F[i, j] = np.dot(dfdpi, dfdpj)
                 F[i, j] = np.dot(dfdpi[self.mask], dfdpj[self.mask])
+                # if F[i,j] < 0 :
+                #     print(F[i,j])
+                #     print(dfdpi[self.mask])
+                #     print(dfdpj[self.mask])
+        #print("fisher information matrix")
+        #print(np.diag(F))
+        #print("Fisher matrix:")
+        #print(F)
+        # print("eigen values:")
+        # print(np.linalg.eig(F))
         return F
 
     def signal_derivative(self, x, x0):
         h = 1.e-4
+        #h = 1.e-3
+        #h = 1.e-2
+        # h = 1.e-1
+        #h = 1.e-5
+        #h = 1.e-6
+        # h = 1.e-7
+        #h = 1.e-8
+        #h = 1.e-9
+        #h = 1.e-10
+        #h = 1.e-15
+        #h = 1.e-20
         zp = 1. + h
-        deriv = (self.measure_signal(**{x: x0 * zp}) - self.measure_signal(**{x: x0})) / (h * x0)
+        #print(x0)
+        #np.gradient
+        #f=np.array([self.measure_signal(**{x: x0}),self.measure_signal(**{x: x0 * zp})])
+        #print(f)
+        #deriv = np.gradient(f, x0 * zp, axis=0)[0]
+        #print(deriv)
+        #5pt stencil
+        #deriv=(-self.measure_signal(**{x: x0+x0*2*h})+8*self.measure_signal(**{x: x0+x0*h})-8*self.measure_signal(**{x: x0-x0*h})+self.measure_signal(**{x: x0-x0*2*h})) / (12*h * x0)
+
+        #central diff
+        deriv=(self.measure_signal(**{x: x0 * zp}) - self.measure_signal(**{x: x0-x0*h})) / (2*h * x0)
+
+        #original
+        #deriv = (self.measure_signal(**{x: x0 * zp}) - self.measure_signal(**{x: x0})) / (h * x0)
+
         return deriv
 
     def measure_signal(self, **kwarg):
@@ -139,18 +294,20 @@ class FisherEstimation:
             frequencies = self.band_frequencies
         else:
             frequencies = self.center_frequencies
+        #print(kwarg)
         N = len(frequencies)
         model = np.zeros(N, dtype=ndp)
         for fnc in self.signals:
             argsp = inspect.getargspec(fnc)
             args = argsp[0][1:]
-            if len(kwarg) and kwarg.keys()[0] in args:
+            if len(kwarg) and list(kwarg.keys())[0] in args:
                 model += fnc(frequencies, **kwarg)
         if self.bandpass:
-            #rmodel = model.reshape((N / self.binstep, self.binstep)) 
+            #rmodel = model.reshape((N / self.binstep, self.binstep))
             #total = rmodel * self.windowfnc
-            return model.reshape((N / self.binstep, self.binstep)).mean(axis=1)
+            return model.reshape((int(N / self.binstep), self.binstep)).mean(axis=1)
             #return total.mean(axis=1)
         else:
+            #plt.loglog(frequencies, model)
+            #print(model)
             return model
-
